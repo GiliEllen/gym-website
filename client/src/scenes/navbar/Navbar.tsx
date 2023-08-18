932acbd54b7686546dfba1d556077556b71f2d14
Gili <gilimena95@gmail.com>
2023-08-18 11:23:03 +0300

    creating action button for late use

--- client/src/scenes/navbar/Navbar.tsx
+++ client/src/scenes/navbar/Navbar.tsx
@@ -3,6 +3,7 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
 import Link from "./Link";
 import { SelectedPage } from "@/shared/types";
+import useMediaQuery from "@/hooks/useMediaQuery";
 
 type Props = {
   selectedPage: SelectedPage;
@@ -10,7 +11,10 @@ type Props = {
 };
 
 const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
+  const [isMenuToggle, setIsMenuToggle] = useState<boolean>(false)
   const flexBetween = "flex items-center justify-between";
+  const isAboveMediumScreen = useMediaQuery("(min-width: 1060px)");
+
   return (
     <nav>
       <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
@@ -20,30 +24,41 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
             <img alt="logo" src={Logo} />
 
             {/*RIGHT*/}
-            <div className={`${flexBetween} w-3/5`}>
-              <Link
-                page="Home"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />
-              <Link
-                page="Benefits"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />
-              <Link
-                page="Our Classes"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />
-              <Link
-                page="Contact Us"
-                selectedPage={selectedPage}
-                setSelectedPage={setSelectedPage}
-              />
+            {isAboveMediumScreen ? (
+              <div className={`${flexBetween} w-3/5`}>
+                <Link
+                  page="Home"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Benefits"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Our Classes"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Contact Us"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
 
-              {/*SIGN In and become a member*/}
-            </div>
+                {/*SIGN In and become a member*/}
+              </div>
+            ) : (
+              <button
+                className="rounded-full bg-secondary-500 p-2"
+                onClick={() => {
+                  setIsMenuToggle(!isMenuToggle);
+                }}
+              >
+                <Bars3Icon className="h-6 w-6 text-white"/>
+              </button>
+            )}
           </div>
         </div>
       </div>
