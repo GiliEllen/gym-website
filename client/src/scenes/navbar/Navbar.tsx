51410debdebb095d6354aea2d7da892a36550945
Gili <gilimena95@gmail.com>
2023-08-18 11:38:24 +0300

    finished navbar

--- client/src/scenes/navbar/Navbar.tsx
+++ client/src/scenes/navbar/Navbar.tsx
@@ -8,16 +8,19 @@ import useMediaQuery from "@/hooks/useMediaQuery";
 type Props = {
   selectedPage: SelectedPage;
   setSelectedPage: (value: SelectedPage) => void;
+  isTopOfPage: boolean;
 };
 
-const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
-  const [isMenuToggle, setIsMenuToggle] = useState<boolean>(false)
+const Navbar = ({ selectedPage, setSelectedPage, isTopOfPage }: Props) => {
+  const [isMenuToggle, setIsMenuToggle] = useState<boolean>(false);
   const flexBetween = "flex items-center justify-between";
   const isAboveMediumScreen = useMediaQuery("(min-width: 1060px)");
 
+  const navbarBackground = isTopOfPage ? "" : "bg-primary-100 drop-shadow"
+
   return (
     <nav>
-      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+      <div className={`${navbarBackground}${flexBetween} fixed top-0 z-30 w-full py-6`}>
         <div className={`${flexBetween} mx-auto w-5/6`}>
           <div className={`${flexBetween} w-full gap-16`}>
             {/*LEFT*/}
@@ -56,12 +59,49 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
                   setIsMenuToggle(!isMenuToggle);
                 }}
               >
-                <Bars3Icon className="h-6 w-6 text-white"/>
+                <Bars3Icon className="h-6 w-6 text-white" />
               </button>
             )}
           </div>
         </div>
       </div>
+
+      {/* MOBILLE MENU MODAL*/}
+      {!isAboveMediumScreen && isMenuToggle && (
+        <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
+          {/* CLOSE ICON */}
+          <div className="flex justify-end p-12">
+            <button onClick={() => setIsMenuToggle(!isMenuToggle)}>
+              <XMarkIcon className="h-6 w-6 text-gray-400" />
+            </button>
+          </div>
+          {/* MENU ITEMS */}
+          <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+            <Link
+              page="Home"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Benefits"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Our Classes"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Contact Us"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+
+            {/*SIGN In and become a member*/}
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
