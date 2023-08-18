85e032e1f7a0fc8ab4721a168ebc827536584bf3
Gili <gilimena95@gmail.com>
2023-08-18 10:57:07 +0300

    adding type to selectedPage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Navbar from "@/scenes/navbar/Navbar";
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState("home");
+  const [selectedPage, setSelectedPage] = useState<string>("home");
   return (
     <div className="app bg-gray-20">
       <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
