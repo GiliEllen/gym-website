286569ec4b3943714af2b86d2d4513f8cacbe688
Gili <gilimena95@gmail.com>
2023-08-20 12:50:55 +0300

    finished home.tsx

--- client/src/scenes/home/Home.tsx
+++ client/src/scenes/home/Home.tsx
@@ -3,6 +3,7 @@ import useMediaQuery from "@/hooks/useMediaQuery";
 import ActionButton from "@/shared/ActionButton";
 import HomePageText from "@/assets/HomePageText.png";
 import HomePageGraphic from "@/assets/HomePageGraphic.png";
+import { motion } from "framer-motion";
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
@@ -14,24 +15,45 @@ const Home = ({ setSelectedPage }: Props) => {
   return (
     <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-4">
       {/* IMAGE AND MAIN HEADER */}
-      <div className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6">
+      <motion.div 
+      className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6"
+      onViewportEnter={() =>setSelectedPage(SelectedPage.Home)}
+      >
         {/* MAIN HEADER */}
         <div className="z-10 mt-32 md:basis-3/5">
           {/* HEADING */}
-          <div className="md:-mt-20">
+          <motion.div
+            className="md:-mt-20"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -50 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
             <div className="relative">
               <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext">
                 <img alt="home-page-text" src={HomePageText} />
               </div>
             </div>
             <p className="mt-8 text-sm">Gym Text</p>
-          </div>
+          </motion.div>
           {/* ACTIONS */}
-          <div className="mt-8 flex items-center gap-8">
+          <motion.div className="mt-8 flex items-center gap-8"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{delay: 0.2, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}>
             <ActionButton setSelectedPage={setSelectedPage}>
               Learn More
             </ActionButton>
-          </div>
+          </motion.div>
         </div>
         {/* IMAGE */}
         <div
@@ -40,7 +62,7 @@ const Home = ({ setSelectedPage }: Props) => {
         >
           <img alt="home-page-graphic" src={HomePageGraphic} />
         </div>
-      </div>
+      </motion.div>
     </section>
   );
 };
